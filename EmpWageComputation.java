a7a3d7106ac32221ba1c87a8dd025a1dc7e864d4
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-20 12:48:44 +0530

    max wage condition

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -8,21 +8,27 @@ public class EmpWageComputation {
 	public final static int PART_TIME_HR = 4; 
 	static int rand = (int) (Math.random() * 3);
 	static int workingDays;
+	static int workingHrs;
+	public final static int TOTAL_WORKING_HRS = 100; 
+	public final static int TOTAL_WORKING_DAYS = 20; 
+
 	static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		System.out.println("*****Welcome To Employee Wage Computation*****\n");
 		
-		monthlyWage();
+		totalWage();
 	}
 	
-	public static void monthlyWage() {
+	public static void totalWage() {
 		System.out.println("Enter a working days of employee - ");
 		workingDays = input.nextInt();
+		System.out.println("Enter a working Hours of employee - ");
+		workingHrs = input.nextInt();
 		int dailyEmpWage = PER_HR_EMP_WAGE * HR_PER_DAY;
 		int totalWage = dailyEmpWage * workingDays;
-		if(workingDays <= 20) {
-			System.out.println("working days of employee is " + workingDays +" "+ "and employee wage is "+ totalWage +" "+ "Rs only" );
+		if(workingDays <= TOTAL_WORKING_DAYS || workingHrs <= TOTAL_WORKING_HRS ) {
+			System.out.println("working days of employee is " + workingDays +" "+"and Total working hours is "+ " "+workingHrs+" "+ "and employee wage is "+ totalWage +" "+ "Rs only" );
 		}
 		
 	}
