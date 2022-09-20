552370f0b94d99499922c2b0ca497bb588dc6e52
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-20 12:29:59 +0530

    Monthly Employee Wage computation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,31 +1,28 @@
 package com.bridgelabz.empwageoops;
 
+import java.util.Scanner;
+
 public class EmpWageComputation {
 	public final static int PER_HR_EMP_WAGE = 20; 
 	public final static int HR_PER_DAY = 8; 
 	public final static int PART_TIME_HR = 4; 
 	static int rand = (int) (Math.random() * 3);
+	static int workingDays;
+	static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		System.out.println("*****Welcome To Employee Wage Computation*****\n");
 		
-		empWage();
+		monthlyWage();
 	}
 	
-	public static void empWage() {
-		switch(rand) {
-		case 1:
-			int dailyEmpWage = PER_HR_EMP_WAGE * HR_PER_DAY;
-			System.out.println("Employee is Present and wage is" +" "+ dailyEmpWage + " "+"Rs Only");
-			break;
-			
-		case 2: 
-			int isWage = PER_HR_EMP_WAGE * (HR_PER_DAY + PART_TIME_HR);
-			System.out.println("Employee is Present Full time as weel as Part time and wage is " + " "+ isWage + " "+ "Rs only");
-			break;
-		
-		default:
-			System.out.println("Employee is absent");
+	public static void monthlyWage() {
+		System.out.println("Enter a working days of employee - ");
+		workingDays = input.nextInt();
+		int dailyEmpWage = PER_HR_EMP_WAGE * HR_PER_DAY;
+		int totalWage = dailyEmpWage * workingDays;
+		if(workingDays <= 20) {
+			System.out.println("working days of employee is " + workingDays +" "+ "and employee wage is "+ totalWage +" "+ "Rs only" );
 		}
 		
 	}
