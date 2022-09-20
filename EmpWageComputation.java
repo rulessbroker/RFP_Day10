8c461a798ae147f0c4bb48f7740a699f81173d35
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-20 11:37:43 +0530

    Employee Check

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.empwageoops;
 
+import java.util.Random;
+
 public class EmpWageComputation {
 	public static void main(String[] args) {
 		System.out.println("*****Welcome To Employee Wage Computation*****");
-
+		
+		EmpWageComputation.checkEmp();
+	}
+	
+	public static void checkEmp() {
+		int rand = (int) (Math.random() * 2);
+		
+		if(rand == 1) {
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
