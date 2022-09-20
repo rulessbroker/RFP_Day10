2a633aa1dc19741ef8b5077e54289f60f305a88c
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-20 11:50:46 +0530

    Employee Part-Time Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,14 +3,18 @@ package com.bridgelabz.empwageoops;
 public class EmpWageComputation {
 	public final static int PER_HR_EMP_WAGE = 20; 
 	public final static int HR_PER_DAY = 8; 
+	public final static int PART_TIME_HR = 4; 
+
 	
 	
 	public static void main(String[] args) {
-		System.out.println("*****Welcome To Employee Wage Computation*****");
+		System.out.println("*****Welcome To Employee Wage Computation*****\n");
 		
 		checkEmp();
 		
 		dailyWage();
+		
+		partTimeWage();
 	}
 	
 	public static void checkEmp() {
@@ -27,6 +31,13 @@ public class EmpWageComputation {
 		int dailyEmpWage = PER_HR_EMP_WAGE * HR_PER_DAY;
 		
 		System.out.println("\nDaily Employee Wage is" +" " + dailyEmpWage + " "+"Rs Only" );
+	}
+	
+	public static void partTimeWage() {
+		int partTimeEmpWage = PART_TIME_HR * PER_HR_EMP_WAGE;
+		
+		System.out.println("\nPart Time Employee Wage is" +" " + partTimeEmpWage + " "+"Rs Only" );
+
 	}
 
 }
