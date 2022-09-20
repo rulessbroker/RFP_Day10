3878db33d1de547630a36f441d74de80908db0f5
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-20 11:45:51 +0530

    Employee Daily Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.empwageoops;
 
-import java.util.Random;
-
 public class EmpWageComputation {
+	public final static int PER_HR_EMP_WAGE = 20; 
+	public final static int HR_PER_DAY = 8; 
+	
+	
 	public static void main(String[] args) {
 		System.out.println("*****Welcome To Employee Wage Computation*****");
 		
-		EmpWageComputation.checkEmp();
+		checkEmp();
+		
+		dailyWage();
 	}
 	
 	public static void checkEmp() {
@@ -17,6 +21,12 @@ public class EmpWageComputation {
 		}else {
 			System.out.println("Employee is Absent");
 		}
+	}
+	
+	public static void dailyWage() {
+		int dailyEmpWage = PER_HR_EMP_WAGE * HR_PER_DAY;
+		
+		System.out.println("\nDaily Employee Wage is" +" " + dailyEmpWage + " "+"Rs Only" );
 	}
 
 }
