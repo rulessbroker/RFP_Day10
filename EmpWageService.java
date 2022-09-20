e72b9ab181b39e9d5a20de9d6f99602eacda3e4d
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-20 13:20:23 +0530

    adding interface

---
+++ EmpWageService.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.empwageoops;
+
+public interface EmpWageService {
+	 void getWageByCompany();
+
+	  void addCompanyEmpWage(String company, short empWagePerHr, short daysToWork, short hrsToWork);
+
+	  void getMonthlyWage();
+
+	  short getMonthlyWage(CompanyEmpWage c);
+
+	  short getDailyWage(short empWagePerHr);
+
+	  String checkAttendance();
+	}
+
